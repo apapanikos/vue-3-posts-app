0b7dc29eefe0a9a5e8170373af15a9e46017deba
Alex Papanikos <alexpapanikos@Alexs-MacBook-Pro.local>
2023-06-25 00:54:39 +0300

    style(homeview): include post card and style home view

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="flex items-center justify-between py-10">
+    <div>
+      <RouterLink :to="`/`">
+        <div class="flex items-center justify-between">VueLog 3</div>
+      </RouterLink>
+    </div>
+    <div class="flex items-center text-base leading-5">
+      <div class="hidden sm:block">
+        <div class="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4">
+          <RouterLink :to="`/authors`">Authors</RouterLink>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
