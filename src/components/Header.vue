8859ecddfbfca018a5d39b5a159dce94a0a33058
Alex Papanikos <alexpapanikos@Alexs-MacBook-Pro.local>
2023-06-25 01:26:21 +0300

    fix(homeview): include vue logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,21 @@
   <header class="flex items-center justify-between py-10">
     <div>
       <RouterLink :to="`/`">
-        <div class="flex items-center justify-between">VueLog 3</div>
+        <div class="flex items-center justify-between">
+          <div class="mr-3 w-[25px]">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 261.76 226.69">
+              <path
+                d="M161.096.001l-30.225 52.351L100.647.001H-.005l130.877 226.688L261.749.001z"
+                fill="#41b883"
+              />
+              <path
+                d="M161.096.001l-30.225 52.351L100.647.001H52.346l78.526 136.01L209.398.001z"
+                fill="#34495e"
+              />
+            </svg>
+          </div>
+          <div class="hidden h-6 text-2xl font-semibold sm:block">VueVlog3</div>
+        </div>
       </RouterLink>
     </div>
     <div class="flex items-center text-base leading-5">
